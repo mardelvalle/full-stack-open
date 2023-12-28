5d6906552814d933afd9a9b479687c212bb7320b
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2023-12-28 03:04:33 -0500

    Complete 1.12

--- part1/part1.14/src/App.jsx
+++ part1/part1.14/src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
-import Button from './Button'
-import StatisticLine from './StatisticLine'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const numberGenerator = () => {
+    console.log(selected)
+    setSelected(Math.floor(Math.random() * 8))
+  }
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <Button total={total} setTotal={setTotal} option={good} setOption={setGood} text="good" />
-      <Button total={total} setTotal={setTotal} option={neutral} setOption={setNeutral} text="neutral" />
-      <Button total={total} setTotal={setTotal} option={bad} setOption={setBad} text="bad" />
-      <h2>Statistics</h2>
-      { total > 0 ? (
-        <table>
-          <StatisticLine text="good" stat={good} />
-          <StatisticLine text="bad" stat={bad} />
-          <StatisticLine text="neutral" stat={neutral} />
-          <StatisticLine text="average" stat={(good - bad)/total} />
-          <StatisticLine text="total" stat={total} />
-          <StatisticLine text="percent positive" stat={good/total} />
-        </table>
-      ) : <p>No feedback given</p>
-      }
+      <p>{anecdotes[selected]}</p>
+      <button onClick={() => numberGenerator()}>next anecdotes</button>
     </div>
   )
 }
