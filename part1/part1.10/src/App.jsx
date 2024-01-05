02f8c418f10ff65cf4764e2e061fb023cae5cca0
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2024-01-05 08:26:11 -0500

    Create 1.10 folder

---
+++ part1/part1.10/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import StatisticLine from './StatisticLine'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+
+  const increasePoints = (option, setOption) => {
+      setOption(option + 1)
+      setTotal(total + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => increasePoints(good, setGood)}>good</button>
+      <button onClick={() => increasePoints(neutral, setNeutral)}>neutral</button>
+      <button onClick={() => increasePoints(bad, setBad)}>bad</button>
+      <h2>Statistics</h2>
+      { total > 0 ? (
+        <>
+          <StatisticLine text="good" stat={good} />
+          <StatisticLine text="bad" stat={bad} />
+          <StatisticLine text="neutral" stat={neutral} />
+          <StatisticLine text="average" stat={(good - bad)/total} />
+          <StatisticLine text="total" stat={total} />
+          <StatisticLine text="percent positive" stat={good/total} />
+        </>
+      ) : <p>No feedback given</p>
+      }
+    </div>
+  )
+}
+
+export default App
