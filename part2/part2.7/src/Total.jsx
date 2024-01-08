d9bd65b4d723215b0af451d291e7d7ae15255195
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2024-01-08 08:46:42 -0500

    One more removed

--- part2/part2.7/src/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <h3>{`Total of ${total} exercises`}</h3>
-  )
-}
-
-export default Total
